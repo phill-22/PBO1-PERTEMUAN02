dafd07690bf778e12e46f6eb720eec0c2e2f2dcd
parzv <muhammadfajar3064@gmail.com>
2024-03-26 20:18:43 +0800

    Buat Class latihan dan Database

---
+++ koneksi/database.java
@@ -0,0 +1,8 @@
+package koneksi;
+
+public class database {
+  public static void hubungkan() {
+    System.out.println("Database terhubung");
+
+  }
+}
---
+++ latihan.java
@@ -0,0 +1,6 @@
+public class latihan {
+
+    public static void main(String[] args) {
+        System.out.println("ini main program");
+    }
+}

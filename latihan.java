536d3bb29818ebe91fae4743c8ef79fbc180dd79
parzv <muhammadfajar3064@gmail.com>
2024-03-26 20:21:55 +0800

    memanggil method dari class di package lain

--- latihan.java
+++ latihan.java
@@ -1,6 +1,9 @@
+import koneksi.database;
+
 public class latihan {
 
     public static void main(String[] args) {
         System.out.println("ini main program");
+        database.hubungkan();
     }
 }
